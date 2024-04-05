8029096ea157184a3e2b421d1539b90a27475854
Nyaradzo Chinyati <nchinyati20@gmail.com>
2024-04-05 21:07:25 +0200

    yakanzi new html ndiyo yangandichikuratidzai that time

---
+++ corlate website/thewebsite/website.js
@@ -0,0 +1,7 @@
+ScrollReveal({reset: true });
+
+ScrollReveal().reveal('h1', { delay: 1000, origin: 'bottom' });
+
+ScrollReveal().reveal('.image1 ', { delay: 1500, origin: 'bottom' });
+ScrollReveal().reveal('.button1', { delay: 2000, origin: 'bottom' });
+ScrollReveal().reveal('.image', { delay: 2000, origin: 'bottom' });
